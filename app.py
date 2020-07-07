945df32f8f1aa3e85fbaa9fcc4bdfa522991088e
Robert-Siberry <robert.siberry@outlook.com>
2020-07-07 11:19:09 +0100

    updated and added now code and functions and an article about canning in funny colours

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+dummyData = [
+    {
+        "f_name": "Robert",
+        "l_name": "Siberry",
+        "title": "Dr",
+        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        "f_name": "Pete",
+        "l_name": "Repeat",
+        "title": "Mr",
+        "content": "Pete and Repeat are on a boat Pete fell out who was left?"
+    }
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/home')
+def home():
+    return render_template('homepage.html', title='Homepage', posts=dummyData)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':

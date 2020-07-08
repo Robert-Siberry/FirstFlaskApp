d931a16757618b2f5a4596476a2dd277d57ee44a
Robert-Siberry <robert.siberry@outlook.com>
2020-07-08 16:13:32 +0100

    following Flask Validation and Secrets on Qa community

--- app.py
+++ app.py
@@ -2,29 +2,20 @@ from flask import Flask, redirect, url_for
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
+from wtforms import ValidationError
+from forms import PostsForm, RegistrationForm
+from flask_bcrypt import Bcrypt
 
-#my IP 77.100.120.192
-#token hex aac919961fe858a46dba9c060cf7fc12
-from forms import PostsForm
+bcrypt = Bcrypt()
 
 app = Flask(__name__)
-#aac919961fe858a46dba9c060cf7fc12
-#make more secure
+
 app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
-#app.config['SLQALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@34.89.109.23:3306/posts'
+app.config['SLQALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@34.89.109.23:3306/posts'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
-                                        environ.get('MYSQL_USER') + \
-                                        ':' + \
-                                        environ.get('MYSQL_PASSWORD') + \
-                                        '@' + \
-                                        environ.get('MYSQL_HOST') + \
-                                        ':' + \
-                                        environ.get('MYSQL_PORT') + \
-                                        '/' + \
-                                        environ.get('MYSQL_DB_NAME')
 db = SQLAlchemy(app)
+
 
 class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,12 +28,41 @@ class Posts(db.Model):
         return ''.join(
             [
                 'Title: ' + self.title + '\n'
-                'First name: ' +self.f_name, + ' ' + self.l_name +'\n'
-                'Content: ' +self.content
+                                         'First name: ' + self.f_name, + ' ' + self.l_name + '\n'
+                                                                                             'Content: ' + self.content
             ]
         )
 
 
+def validate_email(email):
+    user = Users.query.filter_by(email=email.data).first()
+
+    if user:
+        raise ValidationError('Email already in use')
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(500), nullable=False, unique=True)
+    password = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return ''.join(['UserID: ', str(self.id), '\r\n', 'Email: ', self.email])
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hash_pw = bcrypt.generate_password_hash(form.password.data)
+
+        user = Users(email=form.email.data, password=hash_pw)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('post'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/')
@@ -55,6 +75,7 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -72,11 +93,14 @@ def add():
     else:
         return render_template('post.html', title='add a post', form=form)
 
+
 @app.route('/create')
 def create():
+    db.drop_all()
     db.create_all()
     post = Posts(f_name='Robert', l_name='Siberry', title='Dr', content="An interesting canning article")
-    post2 = Posts(f_name='Pete', l_name='Repeat', title='Mr', content="Pete and Repeat where on a boat, Pete fell out who was left?")
+    post2 = Posts(f_name='Pete', l_name='Repeat', title='Mr',
+                  content="Pete and Repeat where on a boat, Pete fell out who was left?")
     db.session.add(post)
     db.session.add(post2)
     db.session.commit()
@@ -84,9 +108,8 @@ def create():
 
 
 @app.route('/delete')
-
 def delete():
-    #db.drop_all()
+    # db.drop_all()
     db.session.query(Posts).delete()
     db.session.commit()
     return "You have deleted everything, now the world is going to end!!!!!"
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class PostsForm(FlaskForm):
@@ -37,3 +37,24 @@ class PostsForm(FlaskForm):
     )
 
     submit = SubmitField('Submit a Post')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',
+        validators = [
+            DataRequired(),
+            Email()
+        ]
+    )
+    password = PasswordField('Password',
+        validators = [
+            DataRequired(),
+        ]
+    )
+    confirm_password = PasswordField('Confirm Password',
+        validators = [
+            DataRequired(),
+            EqualTo('password')
+        ]
+    )
+    submit = SubmitField('Sign Up')
